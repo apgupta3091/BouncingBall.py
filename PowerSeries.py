c1e68ec19558a970ff3e68434430ba81cc2ad0c8
apgupta3091 <53449807+apgupta3091@users.noreply.github.com>
2019-12-05 00:52:18 -0500

    Add files via upload
    Power series where you enter the denominator and the number of numbers in the series and the program will return
    each value plus the last value into the formula serie=1/den**k, and the final sum + the final sum subtracted from one.

---
+++ PowerSeries.py
@@ -0,0 +1,43 @@
+# Official Name: Anuj Gupta
+#Lab section:M5
+#email:apgupta@syr.edu
+#Assignment 3, problem 1
+#Date: 9/19/2019
+#PowerSeries.py
+#Main program that asks for the number of terms, then uses that
+#number of terms to compute a series with user inputed denominator
+#that finally subtracts the last num in series-1 to see how close
+#series gets to 1.
+
+
+#Main program
+def main():
+
+
+    #Set k and the sum=0 outside the loop
+    k=0
+    sum=0
+
+    #Ask for the user input of the denominator as well as the number of terms
+    den,terms=eval(input("Enter denominator and the number of terms:"))
+
+    #Create a loop that computes the series with the given denominator
+    #and for the given amount of terms provided by the user
+    
+    #This series is =1/denominator**1+1/denominator**2...
+    for i in range (1,(terms+1)):
+        k=k+1
+        serie=1/den**k
+        
+        #Get the sum for each term and print every single one
+        sum=serie+sum
+        print(k,sum)
+
+    #print the final sum outside of the loop
+    print("sum =",sum)
+    
+    #Calculate (1/(denominator-1)-sum) and print it
+    print("(1/",den-1,")-sum =",1/(den-1)-sum)
+
+#Close the main program.
+main()
