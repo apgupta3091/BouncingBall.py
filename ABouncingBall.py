af3d6c09765b9919a0c180887d469f08d0b87355
Anuj Gupta <53449807+apgupta3091@users.noreply.github.com>
2020-11-13 14:29:05 -0500

    Update a BouncingBall.py
    Added comments to a BouncingBall.py file to help understand my code for future use.

--- ABouncingBall.py
+++ ABouncingBall.py
@@ -1,5 +1,9 @@
+#import the graphics library
 from graphics import *
+#import randrange from the random library
 from random import randrange
+
+# BBall class created to create different balls of different color/location
 class BBall:
     def __init__(self,color,center,dir=1):
         
@@ -8,18 +12,20 @@ class BBall:
         self.direction=dir
         
         
-
+    #Function draws the circle/balls in the window
     def draw(self,win):
         self.circle.draw(win)
-
+    #Function moves the ball within the window 
     def move(self,win):
         
+        # If the center of the Y of the circle is = 9 or -9, then set self.direction to 1 or -1
+        #else keep the self.direction as 1
         if self.circle.getCenter().getY()==9:
             self.direction=-1
         elif self.circle.getCenter().getY()==-9:
             self.direction=1
         self.circle.move(0,self.direction)
-
+    # Function changes the color of the balls based on the random position generated in the num variable
     def changeColor(self):
         colors=["red","orange","yellow","green","blue","indigo","violet"]
         num=randrange(0,len(colors))
@@ -33,28 +39,39 @@ class BBall:
     
 
 def main():
+    #Creates the graphics window with the title A Bouncing Ball with the coordinates mentioned below
     win=GraphWin("A Bouncing Ball",750,750)
     win.setBackground("tan")
     win.setCoords(-10,-10,10,10)
-
+    
+    #Creates each ball of different colors from the BBall class and draws them into the window
     c=Point(0,-9)
     bouncyBall=BBall("red",c)
     bouncyBall.draw(win)
+    
     d=Point(5,9)
     downBall=BBall("blue",d)
     downBall.draw(win)
+    
     e=Point(-3,0)
     midUp=BBall("green",e)
     midUp.draw(win)
+    
     f=Point(-6,0)
     midDown=BBall("yellow",f,-1)
     midDown.draw(win)
     
+    #While loop that checks if the user has clicked the letter 'q'
+    #If clicked, then the window will be closed
+    #If not clicked then the balls will be in continuos motion in the window
     while win.checkKey()!='q':
         bouncyBall.move(win)
         downBall.move(win)
         midUp.move(win)
         midDown.move(win)
+        
+        #If the mouse is clicked, then the balls will randomly change color with the changeColor() function
+        #else nothing will change with the balls when the mouse isn't clicked
         if win.checkMouse()!=None:
             bouncyBall.changeColor()
             downBall.changeColor()
@@ -62,6 +79,6 @@ def main():
             midDown.changeColor()
             
     win.close()
-
+#close main
 main()
     
