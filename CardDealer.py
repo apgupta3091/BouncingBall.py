84ff662dbf8c730f84a8c915de2bfa4d37029900
Anuj Gupta <53449807+apgupta3091@users.noreply.github.com>
2020-11-15 20:52:20 -0500

    Update CardDealer.py

--- CardDealer.py
+++ CardDealer.py
@@ -2,53 +2,35 @@
 #lab section m5
 #Lab 16
 #SimplisticCardDealer.py
-
 #simulate dealing a hand of cards. (may have repeats)
 
-#import library so we can use randrange
+#import randrange from random library
 from random import randrange
 
-
-#return a random rank for a card, that is
-#returns a random character from "A23456789TJQK"
-#Notice the use of T for ten
+#return a random rank for a card
+#returns a random character from "A23456789TJQK" as the rank
 def chooseRank():
     ranks="A23456789TJQK"
     r=randrange(13)
     return ranks[r]
-    
-
-
-#return a random suit for a card, that is
-#return a random chaacter from "CDHS"
+   
+#return a random suit for a card
+#return a random chaacter from "CDHS" as the suit
 def chooseSuit():
     suits="CDHS"
     r=randrange(4)
     return suits[r]
     
-
-#create a hand of 7 cards.
-
-def main():
-        
+#creates a hand of 7 cards.
+def main():     
     #initialize hand as an empty list
     hand=[]
-    
-    #repeat 7 times, once for each card
-    for i in range(7):
-            
-    #pick a random rank and suit
+    # for loop to create the random 7 cards and append them to the hand
+    for i in range(7):       
         rank=chooseRank()
         suit=chooseSuit()
-
-    #append a string with that rank and suit
-    #to the list hand
         hand.append(rank+suit)
-
-    #print the hand
     print("The hand after 7 cards are chosen at random is as follows"+"\n",hand)
-
-    #sort the hand and print it again
+    #sort the hand 
     hand.sort()
-
 main()
