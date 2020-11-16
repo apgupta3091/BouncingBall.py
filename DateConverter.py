a9c529df20bd8d1493a293f403ae56b3b19e2f3f
Anuj Gupta <53449807+apgupta3091@users.noreply.github.com>
2020-11-15 20:53:41 -0500

    update DateConverter.py

--- DateConverter.py
+++ DateConverter.py
@@ -9,10 +9,13 @@ def main():
     #Grabs the abbreviation from the months var based on the postion of month var
     pos=(month-1)*3
     monthsAbbrev=months[pos:pos+3]
+    
     #Grabs the day from the user inputted String
     day=int(date[3:5])
+    
     #Grabs the Year from the user inputted String
     year=(date[6:])
+    
     # Prints the Month + day + year in a new format then what the user entered (mm/dd/yy)        
     print(monthsAbbrev+".",day,"20"+year)
     
